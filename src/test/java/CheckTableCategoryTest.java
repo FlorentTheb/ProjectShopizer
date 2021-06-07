da04f40f42c3eaaa04fe48a27dbb3dc7db8ddf67
formation <florenthebault@gmail.com>
2021-06-08 00:09:54 +0200

    add quit to test case on driver

--- src/test/java/CheckTableCategoryTest.java
+++ src/test/java/CheckTableCategoryTest.java
@@ -55,8 +55,8 @@ public class CheckTableCategoryTest extends AbstractTest {
 		assertEquals(true, page_item.checkItemProps(driver));
 	}
 	
-//	@After
-//	public void teardown() {
-//		driver.quit();
-//	}
+	@After
+	public void teardown() {
+		driver.quit();
+	}
 }
